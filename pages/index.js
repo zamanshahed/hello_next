e7a039e0d5a337bf2d5ae9241c206a2d5141cb3f
zamanshahed <shaheduz.zaman@northsouth.edu>
2022-09-26 12:52:25 +0600

    initial setup

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Hey Mom! See, I am Learning <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
